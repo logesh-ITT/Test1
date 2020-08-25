edc27802b2db263ae8ef5f788bd26d80999766e9
Logesh K <logesh.k@intimetec.com>
2020-08-25 10:14:37 +0530

    Updated Calculate code

--- Week1/Calculate_Age.ts
+++ Week1/Calculate_Age.ts
@@ -1,7 +1,15 @@
-function Age(dateofbirth) 
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  readline.question('Enter your Age format Year-Month-Date' + " ", Aage => {
+    console.log(`Hey there ${Aage}!`);
+    function Age(Aage) 
 {
-    var age = Date.now() - dateofbirth.getTime();
+    var age = Date.now() - Aage.getTime();
     var given = new Date(age); 
     return Math.abs(given.getUTCFullYear() - 1970);
 }
-console.log(Age(new Date(1992, 2, 28)));
+console.log(Age(new Date(Aage)));
+readline.close();
+});
