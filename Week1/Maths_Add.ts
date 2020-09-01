c7251122b82f2be796643807cdd762a0dc5f077d
Logesh K <logesh.k@intimetec.com>
2020-09-01 11:51:54 +0530

    deleted unwanted files

--- Week1/Maths_Add.ts
+++
@@ -1,6 +0,0 @@
-function add(a:number,b:number)
-{
-    return a+b;
-}
-var sum = add(2,9)
-console.log(sum);
--- Week1/Variable_Declaration.ts
+++
@@ -1,5 +0,0 @@
-var studentName:string='logesh'
-var studentId:number=2002;
-
-console.log(studentName);
-console.log(studentId);
