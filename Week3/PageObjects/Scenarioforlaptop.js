399fc89681607b3b119a2d29ac7276b2a80fcbfd
Logesh K <logesh.k@intimetec.com>
2020-09-01 14:07:48 +0530

    worked on reviewed comments

--- Week3/PageObjects/Scenarioforlaptop.js
+++ Week3/PageObjects/Scenarioforlaptop.js
@@ -1,15 +1,13 @@
 /// <reference types="Cypress" />
-class Scenarioforlaptop {
-
+class ScenarioForLaptop {
     verifyLaptops() {
-
         cy.contains('Product Finder').trigger('mouseover'); //mouseover to product finder
         cy.get('div>ul>li:nth-child(10)>ul>li:nth-child(3)>a').click({ force: true }); //click the laptop button
         cy.title().should('eq', 'Laptops: Latest & Best Laptops online 2020, Laptop Finder with Price'); //validate the title
         cy.get('#brand_Dell').check().should('be.checked'); //click the checkbox
         cy.get('div:nth-child(1)>div._lpdscn>h3>a').first().click({ force: true }); //click the dell laptop
         return this
-
     }
 }
-export default Scenarioforlaptop;
+export default ScenarioForLaptop;
+
--- Week3/TestCase/GadjectTestcase.js
+++ Week3/TestCase/GadjectTestcase.js
@@ -1,26 +1,22 @@
 /// <reference types="Cypress" />
-import ScenarioforMobilephone from '../PageObjects/ScenarioforMobilephone';
-import Scenarioforlaptop from '../PageObjects/Scenarioforlaptop';
+import ScenarioforMobilephone from '../PageObjects/ScenarioForMobilePhone';
+import Scenarioforlaptop from '../PageObjects/ScenarioForLaptop';
 
 describe("Testcases", function () {
     beforeEach(function () {
         cy.visit('https://gadgets.ndtv.com/'); //Homepage
         cy.get('div > div.pricee-store > picture > img').should('be.visible'); //validate image visibe
-    })
-
+    });
     it('Verify Laptops in ProductFinder', function () {
-        const laptop = new Scenarioforlaptop();
+        const laptop = new ScenarioForLaptop();
         laptop.verifyLaptops()
-        cy.wait(5000);
+        cy.wait(5000);s
         cy.title().should('eq', 'Dell Inspiron 5378 Price (01 Sep 2020) Specification & Reviews । Dell Laptops');
-
-    })
+    });
     it('Compare Mobile phones', function () {
-        const Mobile = new ScenarioforMobilephone();
+        const Mobile = new ScenarioForMobilePhone();
         Mobile.compareMobiles();
-        cy.title().should('eq','Compare Samsung Galaxy M31s vs Apple iPhone X (256GB) Price, Specs, Ratings');
+        cy.title().should('eq', 'Compare Samsung Galaxy M31s vs Apple iPhone X (256GB) Price, Specs, Ratings');
+    });
 
-
-    })
-
-})
+});
